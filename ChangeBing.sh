6c5537dc31c487226ae556f2121a39ed21a88a61
aihdde <58286658+aihdde@users.noreply.github.com>
2023-04-24 15:47:54 +0800

    Update ChangeBing.sh
    修改桌面壁纸生效模式，见注释；修改下载方式，如res设置为raw，同时下载4K图片和RAW图片；仅对7.11测试。

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,28 +1,34 @@
+#user 群晖 NAS 用户名，设置替换桌面壁纸与保存图片文件权限 
+user="admin"
+
+#如需设置用户桌面壁纸为每日必应美图，去掉下面 wallpaper 注释 
+#DSM 管理页面随意指定一张图片为壁纸，并在创建 wallpaper_dir 目录以及 wallpaper、wallpaper_hd 文件 
+wallpaper="/usr/syno/etc/preference/$user/wallpaper_dir/wallpaper"
+wallpaper_hd="/usr/syno/etc/preference/$user/wallpaper_dir/wallpaper_hd"
+
 # 设置你的语言
 # set your language(en-US,zh-CN...)
 lang="zh-CN"
+
 # 如需收集保存壁纸,请去掉下面注释,设置保存文件夹路径
 # 在FileStation里面右键文件夹属性可以看到路径
 # If you want to collect and save Wallpapers, 
 # please remove the comment below and set the savepath.
 # Right click the folder property in FileStation to see the path.
-
-#savepath="/volume1/myshare/wallpaper"
+savepath="/volume1/media/wallpaper"
 
 # 如需下载4k分辨率,请设置res=4k
 # 如需下载体积更大的4k以上分辨率的原始图片,请设置res=raw
 # To download 4K resolution, set res=4K
 # To download a larger original picture, set res=raw
-
-#res=4k
+res=raw
 
 # 修改用户桌面壁纸,注释后会替换系统的wallpaper1
 # 你需要清空浏览器缓存查看效果，仅在DSM7.x上测试.
 # Modify user desktop wallpaper.Only test for DMS7.x.
 # System "Wallpaper1" will replaced by remove the comment.
 # You need to clear the browser cache to see the effect.
-
-#desktop=yes
+desktop=yes
 
 echo "[x]Collecting information..."
 pic="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
@@ -32,9 +38,7 @@ fi
 pic=$(wget -t 5 --no-check-certificate -qO- $pic --header="cookie:_EDGE_S=mkt=$lang")
 echo $pic|grep -q startdate||exit
 link=$(echo https://www.bing.com$(echo $pic|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
-if [ "$res" == "raw" ]
-then link=$(echo $link|grep -Eo "https://[-=?/._a-zA-Z0-9]+")
-fi
+
 date=$(echo $pic|grep -Eo '"startdate":"[0-9]+'|grep -Eo '[0-9]+'|head -1)
 if [ "$date" == "" ]
 then date=$(date +%Y%m%d)
@@ -42,8 +46,10 @@ fi
 title=$(echo $pic|sed 's/.\+"title":"//g'|sed 's/".\+//g')
 copyright=$(echo $pic|sed 's/.\+"copyright[:" ]\+//g'|sed 's/".\+//g')
 keyword=$(echo $copyright|sed 's/, /-/g'|cut -d" " -f1|grep -Eo '[^()\\/:*?"<>]+'|head -1)
-filename="bing_"$date"_"$keyword".jpg"
-echo "Link:"$link
+filename="bing_"$date"_"$keyword"_4K.jpg"
+
+#echo "Pic:"$pic
+#echo "Link:"$link
 echo "Date:"$date
 echo "Title:"$title
 echo "Copyright:"$copyright
@@ -53,11 +59,20 @@ echo "Filename:"$filename
 echo "[x]Downloading wallpaper..."
 tmpfile=/tmp/$filename
 wget -t 5 --no-check-certificate  $link -qO $tmpfile
+if [ "$res" == "raw" ]
+then link_raw=$(echo $link|grep -Eo "https://[-=?/._a-zA-Z0-9]+") 
+filename_raw="bing_"$date"_"$keyword"_RAW.jpg"
+tmpfile_raw=/tmp/$filename_raw
+wget -t 5 --no-check-certificate  $link_raw -qO $tmpfile_raw
+fi
 ls -lah $tmpfile||exit
 
 echo "[x]Copying wallpaper..."
 if [ "$savepath" != "" ]
 then cp $tmpfile "$savepath"
+	if [ "$res" == "raw" ]
+	then cp $tmpfile_raw "$savepath"
+	fi
 echo "Save:"$savepath
 ls -lah "$savepath"|grep $date
 cd "$savepath"
@@ -80,25 +95,11 @@ rm -rf /usr/syno/etc/login_background*.jpg
 cp -f $tmpfile /usr/syno/etc/login_background.jpg
 ln -sf /usr/syno/etc/login_background.jpg /usr/syno/etc/login_background_hd.jpg
 
-echo "[x]Clean..."
-rm -f /tmp/bing_*.jpg
-
 if [ "$desktop" == "yes" ]
 then echo "[x]Applying user desktop wallpaper..."
-mkdir -p /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/
-mkdir -p /usr/syno/synoman/webman/resources/images/1x/default_wallpaper/
-mkdir -p /usr/syno/synoman/webman/resources/images/default/1x/default_wallpaper/
-mkdir -p /usr/syno/synoman/webman/resources/images/default_wallpaper/
-#7.0
-cp -f /usr/syno/etc/login_background.jpg /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/1x/default_wallpaper/dsm7_01.jpg
-#6.0
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/default/1x/default_wallpaper/default_wallpaper.jpg
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/default/1x/default_wallpaper/dsm6_01.jpg
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/default/1x/default_wallpaper/dsm6_02.jpg
-#5.2
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/default_wallpaper/default_wallpaper.jpg
-#5.1
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/default_wallpaper/01.jpg
-ln -sf /usr/syno/synoman/webman/resources/images/2x/default_wallpaper/dsm7_01.jpg /usr/syno/synoman/webman/resources/images/default_wallpaper/02.jpg
+cp -f /usr/syno/etc/login_background.jpg $wallpaper
+cp -f /usr/syno/etc/login_background.jpg $wallpaper_hd
 fi
+
+echo "[x]Clean..."
+rm -f /tmp/bing_*.jpg
